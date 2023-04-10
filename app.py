254d3fdba9a0893a10195865fa8ce734089b8dfd
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 04:03:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,43 @@ planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")
 sheet = planilha.worksheet("lic1")
 app = Flask(__name__)
 
+@bot.message_handler(commands=['classificar'])
+def classify(message):
+    # open the Google Sheets document
+    sheet = client.open_by_url(doc_url).sheet1
+
+    # get the values from the cells and create a pandas DataFrame
+    data = sheet.get_all_values()
+    headers = data.pop(0)
+    df = pd.DataFrame(data, columns=headers)
+
+    # Classify the data
+    modalidades = df['Modalidade'].value_counts()
+    finalidades = df['Finalidade/Objeto/Serviço'].value_counts()
+    situacoes = df['Situação'].value_counts()
+
+    dispensa = modalidades.get('Dispensa de Licitacao', 0)
+    chamada = modalidades.get('Chamada Publica', 0)
+    convite = modalidades.get('Convite', 0)
+
+    andamento = situacoes.get('andamento', 0)
+    aberto = situacoes.get('em aberto', 0)
+    encerrada = situacoes.get('encerrada', 0)
+
+    # send the response to the user
+    response = f"Dispensa de Licitação: {dispensa}\n"
+    response += f"Chamada Pública: {chamada}\n"
+    response += f"Convite: {convite}\n"
+    response += f"-----------------------------------\n"
+    response += f"Andamento: {andamento}\n"
+    response += f"Em aberto: {aberto}\n"
+    response += f"Encerrada: {encerrada}"
+    bot.send_message(message.chat.id, response)
+
+# define the bot command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Olá, para classificar a sua planilha digite classificar")
 
 def ultimas_promocoes():
   scraper = ChannelScraper()
