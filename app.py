0cf7207952ddcf4bf48fb088b124c711a9b9c7d1
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-09 21:51:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def promocoes():
     conteudo += f"<li>{promocao}</li>"
     return conteudo + "</ul>"
 
- @app.route("/dedoduro")
+@app.route("/dedoduro")
 def dedoduro():
   mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
   requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
