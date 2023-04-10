bf6ccbdac888e784d7fd2519f0e4875b7a7b0d99
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 17:59:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import telebot
 import requests
 from flask import Flask, request
 
-# Set up the Flask app
-app = Flask(__name__)
-
 # Set up the Telegram bot
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 bot = telebot.TeleBot(TELEGRAM_API_KEY)
@@ -16,12 +13,18 @@ TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 
 # Set up Google Sheets API
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
-with open("google_sheets_credentials.json", mode="w") as fobj:
+with open("credenciais.json", mode="w") as fobj:
     fobj.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json_keyfile_name("google_sheets_credentials.json")
+conta = ServiceAccountCredentials.from_json_keyfile_name("credentials.json")
 api = gspread.authorize(conta)
-planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")  # Replace with your Google Sheets key
-sheet = planilha.worksheet("lic1")  # Replace with the name of your worksheet
+planilha_google = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")  # Replace with your Google Sheets key
+sheet = planilha_google.worksheet("lic1")  # Replace with the name of your worksheet
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "olá" #print(resultado_scraper)
 
 # Define a route to handle the /start command
 @app.route('/start', methods=['POST'])
