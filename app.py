02d8eb7908bde68f0210371bf948ae67040fca71
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 14:05:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,39 @@ import requests
 import telebot
 from flask import Flask, request
 from oauth2client.service_account import ServiceAccountCredentials
-from tchan import ChannelScraper
 
 
+# Set up the Telegram bot
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 bot = telebot.TeleBot(TELEGRAM_API_KEY)
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
+
+
+# Set up Google Sheets API
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 with open("credenciais.json", mode="w") as fobj:
-  fobj.write(GOOGLE_SHEETS_CREDENTIALS)
+    fobj.write(GOOGLE_SHEETS_CREDENTIALS)
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
-api = gspread.authorize(conta) 
-planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")
-sheet = planilha.worksheet("lic1")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")  # Replace with your Google Sheets key
+sheet = planilha.worksheet("lic1")  # Replace with the name of your worksheet
+
+
+# Set up Flask app
 app = Flask(__name__)
 
-# define a new route for handling the /classificar command
+
+# Handle the /start command
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    response = "Olá, para classificar a sua planilha digite /classificar"
+    bot.send_message(message.chat.id, response)
+
+
+# Handle the /classificar command
 @app.route("/classificar", methods=["POST"])
 def classificar():
-    
+    # Get data from the Google Sheets document
     data = sheet.get_all_values()
     headers = data.pop(0)
     df = pd.DataFrame(data, columns=headers)
@@ -42,7 +56,7 @@ def classificar():
     aberto = situacoes.get('em aberto', 0)
     encerrada = situacoes.get('encerrada', 0)
 
-    # send the response to the user
+    # Send the response to the user
     response = f"Dispensa de Licitação: {dispensa}\n"
     response += f"Chamada Pública: {chamada}\n"
     response += f"Convite: {convite}\n"
@@ -50,11 +64,9 @@ def classificar():
     response += f"Andamento: {andamento}\n"
     response += f"Em aberto: {aberto}\n"
     response += f"Encerrada: {encerrada}"
-    
-    # use the Telegram API to send the response back to the user
     chat_id = request.json["message"]["chat"]["id"]
     bot.send_message(chat_id, response)
 
-    # return a response to indicate that the request was processed successfully
+    # Return a response to indicate that the request was processed successfully
     return "ok"
-  
+
