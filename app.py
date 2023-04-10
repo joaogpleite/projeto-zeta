7c55b7a749e3f2805349190c18f99e9e224a2aa9
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 17:20:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
-import telebot
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+import telebot
+import requests
+from flask import Flask, request
+
+# Set up the Flask app
+app = Flask(__name__)
 
 # Set up the Telegram bot
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
@@ -18,23 +23,37 @@ api = gspread.authorize(conta)
 planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")  # Replace with your Google Sheets key
 sheet = planilha.worksheet("lic1")  # Replace with the name of your worksheet
 
-# Define function to scrape data from Google Sheets and classify it
-def classificar(update, context):
-    # Get data from Google Sheets
-    data = sheet.get_all_records()
-    df = pd.DataFrame(data)
-    
-    # Count occurrences of keywords in "Modalidade" and "Situação" columns
-    modalidade_counts = df["Modalidade"].str.count("Dispensa de Licitação|Chamada Pública|Convite").sum()
-    situacao_counts = df["Situação"].str.count("encerrada|andamento|em aberto").sum()
-    
-    # Send message with counts to user
-    message = f"Dispensa de Licitação: {modalidade_counts}\nChamada Pública: {modalidade_counts}\nConvite: {modalidade_counts}\n-----------------------------------\nAndamento: {situacao_counts}\nEm aberto: {situacao_counts}\nEncerrada: {situacao_counts}"
-    context.bot.send_message(chat_id=update.message.chat_id, text=message)
+# Define a route to handle the /start command
+@app.route('/start', methods=['POST'])
+def start_command():
+    # Get the chat ID of the user who sent the message
+    chat_id = request.json['message']['chat']['id']
 
-# Set up Telegram handler for "/classificar" command
-bot.add_command_handler("classificar", classificar)
+    # Send a message to the user
+    response_text = "Olá! Para classificar as licitações digite /classificar"
+    requests.post(f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage', json={'chat_id': chat_id, 'text': response_text})
 
-# Start the bot
-bot.polling()
+    return 'OK'
+
+# Define a route to handle the /classificar command
+@app.route('/classificar', methods=['POST'])
+def classificar_command():
+    # Get the chat ID of the user who sent the message
+    chat_id = request.json['message']['chat']['id']
+
+    # Get the data from the Google Sheets document
+    data = sheet.get_all_values()
+    df = pd.DataFrame(data[1:], columns=data[0])
+
+    # Classify the data
+    modalidade_counts = df[df["Modalidade"].str.contains("Dispensa de Licitação|Chamada Pública|Convite")]["Modalidade"].value_counts()
+    situacao_counts = df[df["Situação"].str.contains("encerrada|andamento|em aberto")]["Situação"].value_counts()
+
+    # Format the message
+    response_text = f"Modalidade:\n{modalidade_counts}\n\nSituação:\n{situacao_counts}"
+
+    # Send the message to the user
+    requests.post(f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage', json={'chat_id': chat_id, 'text': response_text})
+
+    return 'OK'
 
