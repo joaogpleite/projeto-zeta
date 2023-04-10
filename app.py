ab5355e31472b509e706c9193984b87e9d31fa06
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 16:59:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
-import pandas as pd
-import gspread
-import requests
 import telebot
-from flask import Flask, request
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 # Set up the Telegram bot
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
@@ -20,47 +18,23 @@ api = gspread.authorize(conta)
 planilha = api.open_by_key("1bmLZIrWU1GG_ikJKRcZNtmmFELcYrBK2dMYqFQIV0Gs")  # Replace with your Google Sheets key
 sheet = planilha.worksheet("lic1")  # Replace with the name of your worksheet
 
-# Set up Flask app
-app = Flask(__name__)
+# Define function to scrape data from Google Sheets and classify it
+def classificar(update, context):
+    # Get data from Google Sheets
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+    
+    # Count occurrences of keywords in "Modalidade" and "Situação" columns
+    modalidade_counts = df["Modalidade"].str.count("Dispensa de Licitação|Chamada Pública|Convite").sum()
+    situacao_counts = df["Situação"].str.count("encerrada|andamento|em aberto").sum()
+    
+    # Send message with counts to user
+    message = f"Dispensa de Licitação: {modalidade_counts}\nChamada Pública: {modalidade_counts}\nConvite: {modalidade_counts}\n-----------------------------------\nAndamento: {situacao_counts}\nEm aberto: {situacao_counts}\nEncerrada: {situacao_counts}"
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
-def coleta_dados_view():
-    data = sheet.get_all_values()
-    headers = data.pop(0)
-    df = pd.DataFrame(data, columns=headers)
-    return df[['Modalidade', 'Situação']]
-    print(df.columns)
+# Set up Telegram handler for "/classificar" command
+bot.add_command_handler("classificar", classificar)
 
-@app.route("/telegram-bot", methods=["POST"])
-def telegram_bot():
-    update = request.json
-    if "message" not in update:
-        return "ok"  # Not a message, ignore it
-    message = update["message"]["text"]
-    chat_id = update["message"]["chat"]["id"]
-    if message == "/start":
-        bot.send_message(chat_id, "Olá, para classificar as licitações digite /classificar")
-    elif message == "/classificar":
-        df = coleta_dados_view()
-        modalidades = df['Modalidade'].value_counts()
-        finalidades = df['Finalidade/Objeto/Serviço'].value_counts()
-        situacoes = df['Situação'].value_counts()
-        dispensa = modalidades.get('Dispensa de Licitação', 0)
-        chamada = modalidades.get('Chamada Pública', 0)
-        convite = modalidades.get('Convite', 0)
-         
-        andamento = situacoes.get('andamento', 0)
-        aberto = situacoes.get('em aberto', 0)
-        encerrada = situacoes.get('encerrada', 0)
-        
-        response = f"Aqui estão as licitações classificadas por Modalidade e Situação:\n" \
-                   f"Chamada Pública: {chamada}\n" \
-                   f"Convite: {convite}\n" \
-                   f"Dispensa de Licitação: {dispensa}\n" \
-                   f"Encerrada: {encerrada}\n" \
-                   f"Em aberto: {aberto}\n" \
-                   f"Andamento: {andamento}"
-        bot.send_message(chat_id, response)
-    return "ok"
+# Start the bot
+bot.polling()
 
-if __name__ == "__main__":
-    app.run(port=5000)
